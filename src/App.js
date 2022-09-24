7ca4cd036578c85135bca48444cf1928886b50c9
Taylor <bKuzzcs@gmail.com>
2022-09-24 17:47:24 -0500

    phase-2-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+{/* Navbar */}
+<nav>React Router</nav>
+
+{/* Header */}
+<header>Show List</header>
+
+{/* Form for shows */}
+<form>Shows</form>
+
+{/* The list of shows  */}
+<li>Backyard / Waco, TX, $100</li>
+<li>Blue Light / Lubbock, TX, $100</li>
+<li>Stubbs / Austin, TX, $120</li>
+<li>Wild West / Cedar Park, TX, $145</li>
+<li>Cooks Garage / Lubbock, TX, $130</li>
+
     </div>
   );
 }
+
+
 
 export default App;
